e9107da0e0c15d31699823279077d9966c738bf9
mohammedRasif <mdrasif3499@gmail.com>
2025-05-10 16:28:14 +0600

    modify some new

--- src/component/DashboardLayout/DashboardLayout.jsx
+++ src/component/DashboardLayout/DashboardLayout.jsx
@@ -1,45 +1,61 @@
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
-import { Menu, X } from "lucide-react"; // Sidebar Toggle Icons
+import { GoSidebarCollapse, GoSidebarExpand } from "react-icons/go";
+
 import DashboardNavbar from "../DashboardNavbar/DashboardNavbar";
 import DashboardSidebar from "../DashboardSidebar/DashboardSidebar";
 
 const DashboardLayout = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
     return (
         <div className="flex h-screen">
             {/* Sidebar */}
             <div
-                className={`bg-[#f5efe8af] dark:bg-[#1E232E] h-full fixed ease-in-out ${isSidebarOpen ? "w-[320px]" : "w-16"
-                    }`}
+                className={`bg-[#f5efe8af] dark:bg-[#1E232E] h-full fixed transition-all duration-300 ease-in-out ${
+                    isSidebarOpen ? "w-[320px]" : "w-18"
+                }`}
             >
-                <div className="h-full flex flex-col justify-between">
+                <div className="h-full flex flex-col">
+                    {/* Toggle Button */}
+                    <button
+                        onClick={toggleSidebar}
+                        className="p-4 flex justify-end"
+                    >
+                        {isSidebarOpen ? (
+                            <GoSidebarExpand className="h-7 w-7 text-gray-600 dark:text-gray-300 cursor-pointer" />
+                        ) : (
+                            <GoSidebarCollapse className="h-7 w-7 text-gray-600 dark:text-gray-300 cursor-pointer mb-9" />
+                        )}
+                    </button>
                     {/* Sidebar Content */}
-                    <DashboardSidebar />
-
-                    
+                    <DashboardSidebar isSidebarOpen={isSidebarOpen} />
                 </div>
             </div>
 
             {/* Main Content Area */}
             <div
-                className={`flex flex-col  ${isSidebarOpen ? "ml-[320px] w-[calc(100%-320px)]" : "ml-16 w-[calc(100%-64px)]"
-                    }`}
+                className={`flex flex-col transition-all duration-300 ease-in-out ${
+                    isSidebarOpen ? "ml-[320px] w-[calc(100%-320px)]" : "ml-18 w-[calc(100%-72px)]"
+                }`}
             >
                 {/* Navbar - Fixed & Only Navbar BG White */}
                 <div
-                    className="fixed top-0 z-50 bg-white dark:bg-[#1E232E]    w-full border-b border-gray-200 dark:border-gray-600 "
+                    className="fixed top-0 z-50 bg-white dark:bg-[#1E232E] w-full border-b border-gray-200 dark:border-gray-600 transition-all duration-300 ease-in-out"
                     style={{
-                        left: isSidebarOpen ? "320px" : "64px",
-                        width: isSidebarOpen ? "calc(100%-320px)" : "calc(100%-64px)",
+                        left: isSidebarOpen ? "320px" : "72px",
+                        width: isSidebarOpen ? "calc(100%-320px)" : "calc(100%-72px)",
                     }}
                 >
                     <DashboardNavbar />
                 </div>
 
                 {/* Outlet (Main Content) */}
-                <div className="h-full mt-16 overflow-auto bg-white dark:bg-[#252c3b]  ">
+                <div className="h-full mt-16 overflow-auto bg-white dark:bg-[#252c3b]">
                     <Outlet />
                 </div>
             </div>
@@ -48,6 +64,3 @@ const DashboardLayout = () => {
 };
 
 export default DashboardLayout;
-
-
-
--- src/component/DashboardSidebar/DashboardSidebar.jsx
+++ src/component/DashboardSidebar/DashboardSidebar.jsx
@@ -1,132 +1,108 @@
 import { NavLink, useLocation } from "react-router-dom";
-import img from "../Navbar/image 3.png";
-import img1 from "../Navbar/Group (2).png";
-import { Bot, Briefcase, Clock, Settings, Users } from "lucide-react";
+import img from "../Navbar/image 3.png"; // Ensure correct path
+import img1 from "../Navbar/Group (2).png"; // Ensure correct path
+import { Bot, Briefcase, Clock, Settings } from "lucide-react";
 import { BiSolidDashboard } from "react-icons/bi";
 import { useDarkMode } from "../../context/ThemeContext";
-import { RiPassPendingLine, RiTeamFill } from "react-icons/ri";
-import { GoAlertFill } from "react-icons/go";
-import { LuCircleFadingPlus } from "react-icons/lu";
+import { RiTeamFill } from "react-icons/ri";
 import { MdCompost } from "react-icons/md";
 
-const DashboardSidebar = () => {
-  const location = useLocation();
-  const isProjectActive = location.pathname.startsWith('/dashboard/Project');
-  const { darkMode, setDarkMode } = useDarkMode();
+const DashboardSidebar = ({ isSidebarOpen }) => {
+    const location = useLocation();
+    const isProjectActive = location.pathname.startsWith("/dashboard/Project");
+    const { darkMode } = useDarkMode();
 
+    // Common NavLink styling function
+    const navLinkStyle = (isActive) => `
+        flex items-center gap-3 px-4 py-3 relative transition-all duration-200 group
+        ${isActive
+            ? "bg-white shadow-sm text-[#00308F] rounded-md scale-105 before:absolute before:left-0 before:top-0 before:h-full before:w-2 before:bg-[#00308F] before:rounded-l-md"
+            : "hover:bg-gray-100 hover:text-[#00308F] rounded-md"
+        }
+        ${isSidebarOpen ? "justify-start" : "justify-center"}
+    `;
 
-  // Common NavLink styling function
-  const navLinkStyle = (isActive) => `
-    flex items-center gap-3 px-6 py-3  relative 
-    ${isActive
-      ? 'bg-white shadow-sm text-[#00308F] rounded-md m-2 scale-105 before:absolute before:left-0 before:top-0 before:h-full before:w-2 before:bg-[#00308F] before:rounded-l-md'
-      : 'hover:bg-gray-100 hover:text-[#00308F] rounded-md mr-3'
-    }
-  `;
-
-  return (
-    <div className="text-[#00308F] dark:text-white">
-      <NavLink to="/">
-        <div className="flex items-center justify-center pt-10 pb-6">
-          <div>
-            <img
-              src={darkMode ? img1 : img}
-              alt="Logo"
-              className="h-[30px] sm:h-[80px] md:h-[34px] w-auto"
-            />
-          </div>
+    return (
+        <div className="text-[#00308F] dark:text-white flex flex-col h-full">
+            <div className="flex items-center justify-center pt-10 pb-6">
+                {isSidebarOpen && (
+                    <NavLink to="/">
+                        <img
+                            src={darkMode ? img1 : img}
+                            alt="Logo"
+                            className="h-[34px] w-auto"
+                        />
+                    </NavLink>
+                )}
+            </div>
+            <div className="flex flex-col gap-1 px-2">
+                <NavLink
+                    to="/dashboard"
+                    end
+                    className={({ isActive }) => navLinkStyle(isActive)}
+                >
+                    <BiSolidDashboard className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
+                    {isSidebarOpen && (
+                        <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-108">Dashboard</h1>
+                    )}
+                </NavLink>
+                <NavLink
+                    to="/dashboard/chat"
+                    className={({ isActive }) => navLinkStyle(isActive)}
+                >
+                    <Bot className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
+                    {isSidebarOpen && (
+                        <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-108">Ai Assistant</h1>
+                    )}
+                </NavLink>
+                <NavLink
+                    to="/dashboard/Project"
+                    className={() => navLinkStyle(isProjectActive)}
+                >
+                    <Briefcase className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
+                    {isSidebarOpen && (
+                        <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-108">Project</h1>
+                    )}
+                </NavLink>
+                <NavLink
+                    to="/dashboard/team"
+                    className={({ isActive }) => navLinkStyle(isActive)}
+                >
+                    <RiTeamFill className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
+                    {isSidebarOpen && (
+                        <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-108">Team</h1>
+                    )}
+                </NavLink>
+                <NavLink
+                    to="/dashboard/post_project"
+                    className={({ isActive }) => navLinkStyle(isActive)}
+                >
+                    <MdCompost className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
+                    {isSidebarOpen && (
+                        <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-108">Post Project</h1>
+                    )}
+                </NavLink>
+                <NavLink
+                    to="/dashboard/manageSubscription"
+                    className={({ isActive }) => navLinkStyle(isActive)}
+                >
+                    <Clock className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
+                    {isSidebarOpen && (
+                        <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-108">Manage subscription</h1>
+                    )}
+                </NavLink>
+                <NavLink
+                    to="/dashboard/setting"
+                    className={({ isActive }) => navLinkStyle(isActive)}
+                >
+                    <Settings className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
+                    {isSidebarOpen && (
+                        <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-108">Setting</h1>
+                    )}
+                </NavLink>
+            </div>
         </div>
-      </NavLink>
-      <div className="flex flex-col gap-1">
-
-        <NavLink
-          to="/dashboard"
-          end
-          className={({ isActive }) => navLinkStyle(isActive)}
-        >
-          <BiSolidDashboard className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
-          <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-105">Dashboard</h1>
-        </NavLink>
-        {/* <NavLink
-          to="/dashboard/createProject"
-          end
-          className={({ isActive }) => navLinkStyle(isActive)}
-        >
-          <LuCircleFadingPlus className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
-          <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-105">Create Project</h1>
-        </NavLink> */}
-        <NavLink
-          to="/dashboard/chat"
-          className={({ isActive }) => navLinkStyle(isActive)}
-        >
-          <Bot className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
-          <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-105">Ai Assistant</h1>
-        </NavLink>
-
-        <NavLink
-          to="/dashboard/Project"
-          className={() => navLinkStyle(isProjectActive)}
-        >
-          <Briefcase className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
-          <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-105">Project</h1>
-        </NavLink>
-
-        {/* <NavLink
-          to="/dashboard/planing"
-          className={({ isActive }) => navLinkStyle(isActive)}
-        >
-          <Users className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
-          <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-105">Planing</h1>
-        </NavLink> */}
-        <NavLink
-          to="/dashboard/team"
-          className={({ isActive }) => navLinkStyle(isActive)}
-        >
-          <RiTeamFill className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
-          <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-105">Team</h1>
-        </NavLink>
-
-        {/* <NavLink
-          to="/dashboard/taskProgress"
-          className={({ isActive }) => navLinkStyle(isActive)}
-        >
-          <RiPassPendingLine className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
-          <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-105">Task Progress</h1>
-        </NavLink> */}
-        <NavLink
-          to="/dashboard/post_project"
-          className={({ isActive }) => navLinkStyle(isActive)}
-        >
-          <MdCompost className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
-          <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-105">Post Project</h1>
-        </NavLink>
-
-        {/* <NavLink
-          to="/dashboard/risks"
-          className={({ isActive }) => navLinkStyle(isActive)}
-        >
-          <GoAlertFill className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
-          <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-105">Risks</h1>
-        </NavLink> */}
-
-        <NavLink
-          to="/dashboard/manageSubscription"
-          className={({ isActive }) => navLinkStyle(isActive)}
-        >
-          <Clock className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
-          <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-105">Manage subscription</h1>
-        </NavLink>
-
-        <NavLink
-          to="/dashboard/setting"
-          className={({ isActive }) => navLinkStyle(isActive)}
-        >
-          <Settings className="h-6 w-6 transition-transform duration-200 group-hover:scale-110" />
-          <h1 className="text-lg font-medium transition-transform duration-200 group-hover:scale-105">Setting</h1>
-        </NavLink>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default DashboardSidebar;
